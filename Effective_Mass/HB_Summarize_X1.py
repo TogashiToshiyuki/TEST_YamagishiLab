076732ef81120acd6444028839018ef7ff15e9de
Toshiyuki Togashi <t.togashi.1226@icloud.com>
2024-11-18 10:40:09 +0900

    HB_Summarize_X1.pyの作成
    進捗
    ・必要なファイルの確認
    
    次やること

---
+++ Effective_Mass/HB_Summarize_X1.py
@@ -0,0 +1,263 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import argparse
+import os
+
+
+def main():
+    args = get_args()
+    EM = EffectiveMass(args)
+
+
+def get_args():
+    help_text = "This is a help text"
+
+    parser = argparse.ArgumentParser(description=help_text, formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('MaterName',
+                        help='Material name')
+    parser.add_argument('--debug', '-d', '-D',
+                        help="Start in debug mode",
+                        action='store_true')
+    args = parser.parse_args()
+    return args
+
+
+class EffectiveMass:
+    def __init__(self, args):
+        self.args = args
+        self.MaterName = args.MaterName
+        self.debug = args.debug
+        self.messages = []
+        self.HelpList = []
+        self.p1Files, self.p2Files, self.p3Files, self.AllFiles, self.Angles = self.File_Set_Check()
+        os.makedirs("./BandInfo", exist_ok=True)
+        os.makedirs("./Figures/BandStructures", exist_ok=True)
+
+    # Displays messages and exits if unexpected behavior is detected.
+    def help_check_exit(self):
+        """
+        Checks for unexpected behaviors and displays accumulated messages.
+
+        This function performs two main tasks:
+
+        1. It displays accumulated messages stored in `self.message`, allowing
+           for effective message management within the program.
+        2. It checks the `HelpList` attribute for any instances of `True`.
+           If `True` is found, indicating an unexpected behavior, the program
+           terminates after displaying an error message.
+
+        :returns: None
+        :raises SystemExit: Terminates the program if `True` is found in
+                            `HelpList`, signaling an abnormal end due to an
+                            unexpected behavior.
+        """
+        self.message_show()
+        if True in self.HelpList:
+            print(f"{Color.RED}{StandardPhrases.AbnormalEnd}{Color.RESET}")
+            exit()
+        else:
+            pass
+        return None
+
+    def message_show(self):
+        """
+        Show messages.
+        :return:
+        """
+        for message in self.messages:
+            print(message)
+        self.messages.clear()
+        return None
+
+    def File_Set_Check(self):
+        """
+        Check if the required files exist.
+        :return:
+        """
+        print(f"{Color.GREEN}Checking the required files...{Color.RESET}")
+        p1Files, p2Files, p3Files = self.getMinTIFileName()
+        AllFiles = self.getAllFileNames()
+        self.help_check_exit()
+        print(f"{Color.GREEN}All required files are available.{Color.RESET}")
+
+        print("\nTI files:")
+        for p1File in p1Files:
+            print(f"\t{p1File}")
+        for p2File in p2Files:
+            print(f"\t{p2File}")
+        for p3File in p3Files:
+            print(f"\t{p3File}")
+
+        print("\nAll files:")
+        for AllFile in AllFiles:
+            print(f"\t{AllFile}")
+
+        Angles = []
+        for AllFile in AllFiles:
+            Angles_temp = self.getAngle(AllFile)
+            Angles = Angles + Angles_temp
+        Angles = list(set(Angles))
+        Angles.sort()
+        if self.debug:
+            print(f"\nAngles: {Angles}")
+
+        print(f"\nGetting the tilt angles...")
+        Tilt_temp = []
+        for p1File in p1Files:
+            Tilt_temp.append(p1File.split("/")[1].split("_")[2])
+        for p2File in p2Files:
+            Tilt_temp.append(p2File.split("/")[1].split("_")[2])
+        for p3File in p3Files:
+            Tilt_temp.append(p3File.split("/")[1].split("_")[2])
+        Tilt_temp = list(set(Tilt_temp))
+        if len(Tilt_temp) != 1:
+            print(f"{Color.RED}\t>>> Error: Tilt angles are not consistent.{Color.RESET}")
+            self.HelpList.append(True)
+        else:
+            print(f"\t>>> Tilt angles: {Color.GREEN}{Tilt_temp[0]}{Color.RESET}")
+
+        return p1Files, p2Files, p3Files, AllFiles, Angles
+
+    def getMinTIFileName(self):
+        Dirs = os.listdir("./")
+        p1Files, p2Files, p3Files = [], [], []
+        for Dir in Dirs:
+            if self.MaterName in Dir and "_results" in Dir:
+                Files_temp = os.listdir(Dir)
+                for File in Files_temp:
+                    if "3molp1" in File and "min-TIs-" in File:
+                        p1Files.append("./" + Dir + "/" + File)
+                    elif "3molp2" in File and "min-TIs-" in File:
+                        p2Files.append("./" + Dir + "/" + File)
+                    elif "3molp3" in File and "min-TIs-" in File:
+                        p3Files.append("./" + Dir + "/" + File)
+            else:
+                pass
+
+        if len(p1Files) == 0:
+            print(f"{Color.RED}\t>>> Error: No files found for 3molp1.{Color.RESET}")
+            self.HelpList.append(True)
+        elif len(p1Files) != 3:
+            print(f"{Color.RED}\t>>> Error: 3molp1 files are not complete.{Color.RESET}")
+            self.HelpList.append(True)
+        else:
+            print(f"\t>>> 3molp1 files: {Color.GREEN}All Available.{Color.RESET}")
+
+        if len(p2Files) == 0:
+            print(f"{Color.RED}\t>>> Error: No files found for 3molp2.{Color.RESET}")
+            self.HelpList.append(True)
+        elif len(p2Files) != 3:
+            print(f"{Color.RED}\t>>> Error: 3molp2 files are not complete.{Color.RESET}")
+            self.HelpList.append(True)
+        else:
+            print(f"\t>>> 3molp2 files: {Color.GREEN}All Available.{Color.RESET}")
+
+        if len(p3Files) == 0:
+            print(f"{Color.RED}\t>>> Error: No files found for 3molp3.{Color.RESET}")
+            self.HelpList.append(True)
+        elif len(p3Files) != 3:
+            print(f"{Color.RED}\t>>> Error: 3molp3 files are not complete.{Color.RESET}")
+            self.HelpList.append(True)
+        else:
+            print(f"\t>>> 3molp3 files: {Color.GREEN}All Available.{Color.RESET}")
+
+        return p1Files, p2Files, p3Files
+
+    def getAllFileNames(self):
+        Dirs = os.listdir("./")
+        AllFiles = []
+        for Dir in Dirs:
+            if self.MaterName in Dir and "_results" in Dir and os.path.isdir(Dir):
+                Files_temp = os.listdir(Dir)
+                for File in Files_temp:
+                    if "3mol" in File and "_all.txt" in File:
+                        AllFiles.append("./" + Dir + "/" + File)
+                    else:
+                        pass
+            else:
+                pass
+
+        if len(AllFiles) == 0:
+            print(f"{Color.RED}\t>>> Error: No files found for 3mol_all.txt{Color.RESET}")
+            self.HelpList.append(True)
+        else:
+            print(f"\t>>> 3mol_all.txt files: {Color.GREEN}All Available.{Color.RESET}")
+        self.help_check_exit()
+
+        return AllFiles
+
+    @staticmethod
+    def getAngle(AllFile):
+        with open(AllFile, "r") as f:
+            lines = f.readlines()
+        Angles_temp = []
+        for line in lines:
+            Contents = line.strip().split()
+            try:
+                Angles_temp.append(float(Contents[1]))
+            except (ValueError, IndexError):
+                pass
+        return Angles_temp
+
+
+class Color:
+    """
+    ANSI escape code for color
+    """
+
+    def __init__(self):
+        self._Color = "Color"
+
+    BLACK = '\033[30m'  # black
+    RED = '\033[31m'  # red
+    GREEN = '\033[32m'  # green
+    YELLOW = '\033[33m'  # yellow
+    BLUE = '\033[34m'  # blue
+    MAGENTA = '\033[35m'  # magenta
+    CYAN = '\033[36m'  # cyan
+    LIME = '\033[92m'  # lime
+    WHITE = '\033[37m'  # white
+    COLOR_DEFAULT = '\033[39m'  # default
+    BOLD = '\033[1m'  # bold
+    UNDERLINE = '\033[4m'  # underline
+    INVISIBLE = '\033[08m'  # invisible
+    REVERSE = '\033[07m'  # reverse
+    BG_BLACK = '\033[40m'  # (background)black
+    BG_RED = '\033[41m'  # (background)red
+    BG_GREEN = '\033[42m'  # (background)green
+    BG_YELLOW = '\033[43m'  # (background)yellow
+    BG_BLUE = '\033[44m'  # (background)blue
+    BG_MAGENTA = '\033[45m'  # (background)magenta
+    BG_CYAN = '\033[46m'  # (background)cyan
+    BG_WHITE = '\033[47m'  # (background)white
+    BG_DEFAULT = '\033[49m'  # (background)default
+    RESET = '\033[0m'  # reset
+
+
+class StandardPhrases:
+    def __init__(self):
+        self._StandardPhrases = "StandardPhrases"
+
+    ProgramAbst = ("\n"
+                   "*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n"
+                   "*   To Search the energetically stable aggregation                      *\n"
+                   "*            for BrickWork (BW) aggregation                             *\n"
+                   "*                                                                       *\n"
+                   "*   The programme requires the following arguments.                     *\n"
+                   "*     - xyz file of the molecule from which the calculations are made.  *\n"
+                   "*                                                                       *\n"
+                   "*   The following files are also required.                              *\n"
+                   "*     - CalcSetting_BW.txt                                              *\n"
+                   "*                                                                       *\n"
+                   "*    Option                                                             *\n"
+                   "*      - Tilt angle can be added to the molecule                        *\n"
+                   "*                          created by Toshiyuki Togashi 2024/11/12      *\n"
+                   "*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n")
+    AbnormalEnd = "\n************* Programme DID NOT terminate successfully. *************\n"
+    HelpText = ("\n"
+                "The required file may not exist.\n"
+                "Please check.")
+
+
+if __name__ == '__main__':
+    main()
