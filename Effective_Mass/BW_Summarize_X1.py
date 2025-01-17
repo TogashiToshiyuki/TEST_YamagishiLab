4986c5a06467122d8c32cb94f8abf39c1c66dc15
yama-lab <t.togashi.1226@icloud.com>
2025-01-17 16:31:15 +0900

    HB_Summarize_02.pyの作成
    進捗
    ・デバッグ
    
    次やること
    ・計算式の確認
    ・デバッグ

--- Effective_Mass/BW_Summarize_X1.py
+++ Effective_Mass/BW_Summarize_X1.py
@@ -1,31 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import argparse
-import datetime
 import functools
-import glob
-import math
 import os
 import time
-
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-from PIL import Image
-from matplotlib.animation import FuncAnimation
-from pptx import Presentation
-from pptx.dml.color import RGBColor
-from pptx.enum.text import MSO_ANCHOR, PP_ALIGN
-from pptx.util import Pt
-from scipy.interpolate import griddata
-from scipy.optimize import differential_evolution
-from tabulate import tabulate
 
 print = functools.partial(print, flush=True)
 
 
 def main():
     args, before = get_args()
+    EM = EffectiveMass(args)
+
+    # Make band information files
+    print("\n**********\nMaking band information files...")
 
 
 def get_args():
@@ -47,6 +35,141 @@ def get_args():
         print(f"{Color.RED}*************** Caution!!! Debug Started!!! ***************{Color.RESET}\n")
 
     return args, before
+
+
+class EffectiveMass:
+    def __init__(self, args):
+        self.args = args
+        self.MaterName = args.MaterName
+        self.debug = args.debug
+        self.gif = args.gif
+        self.messages = []
+        self.HelpList = []
+        self.p1Files, self.AllFiles, self.Other = self.File_Set_Check()
+        os.makedirs("./BandInfo", exist_ok=True)
+        os.makedirs("./Figures/BandStructures", exist_ok=True)
+        os.makedirs("./Figures/EvsAngle", exist_ok=True)
+        os.makedirs("./Figures/TIvsAngle", exist_ok=True)
+        os.makedirs("./Figures/AllData", exist_ok=True)
+        os.makedirs("./Figures/MassvsAngles", exist_ok=True)
+
+    # Displays messages and exits if unexpected behavior is detected.
+    def help_check_exit(self):
+        """
+        Checks for unexpected behaviors and displays accumulated messages.
+
+        This function performs two main tasks:
+
+        1. It displays accumulated messages stored in `self.message`, allowing
+           for effective message management within the program.
+        2. It checks the `HelpList` attribute for any instances of `True`.
+           If `True` is found, indicating an unexpected behavior, the program
+           terminates after displaying an error message.
+
+        :returns: None
+        :raises SystemExit: Terminates the program if `True` is found in
+                            `HelpList`, signaling an abnormal end due to an
+                            unexpected behavior.
+        """
+        self.message_show()
+        if True in self.HelpList:
+            print(f"{Color.RED}{StandardPhrases.AbnormalEnd}{Color.RESET}")
+            exit()
+        else:
+            pass
+        return None
+
+    def message_show(self):
+        """
+        Show messages.
+        :return:
+        """
+        for message in self.messages:
+            print(message)
+        self.messages.clear()
+        return None
+
+    def File_Set_Check(self):
+        print(f"{Color.GREEN}Checking the required files...{Color.RESET}")
+        p1Files = self.getMinTIFileName()
+        AllFiles = self.getAllFileNames()
+        self.help_check_exit()
+        print(f"{Color.GREEN}All required files are available.{Color.RESET}")
+
+        print("\nTI files:")
+        for p1File in p1Files:
+            print(f"\t{p1File}")
+
+        print("\nAll files:")
+        for AllFile in AllFiles:
+            print(f"\t{AllFile}")
+
+        Other = []
+        for AllFile in AllFiles:
+            Other_temp = self.getOther(AllFile)
+            Other = Other + Other_temp
+        Other = list(set(Other))
+        Other.sort()
+        if self.debug:
+            print(f"\nOther: {Other}")
+
+        return p1Files, AllFiles, Other
+
+    def getMinTIFileName(self):
+        Dirs = os.listdir("./")
+        p1Files = []
+        for Dir in Dirs:
+            if self.MaterName in Dir and "_results" in Dir:
+                Files_temp = os.listdir(Dir)
+                for File in Files_temp:
+                    if "3molp1" in File and "min-TIs-" in File:
+                        p1Files.append("./" + Dir + "/" + File)
+
+        if len(p1Files) == 0:
+            print(f"{Color.RED}\t>>> Error: No files found for 3molp1.{Color.RESET}")
+            self.HelpList.append(True)
+        elif len(p1Files) != 3:
+            print(f"{Color.RED}\t>>> Error: 3molp1 files are not complete.{Color.RESET}")
+            self.HelpList.append(True)
+        else:
+            print(f"\t>>> 3molp1 files: {Color.GREEN}All Available.{Color.RESET}")
+
+        return p1Files
+
+    def getAllFileNames(self):
+        Dirs = os.listdir("./")
+        AllFiles = []
+        for Dir in Dirs:
+            if self.MaterName in Dir and "_results" in Dir and os.path.isdir(Dir):
+                Files_temp = os.listdir(Dir)
+                for File in Files_temp:
+                    if "3mol" in File and "_all.txt" in File:
+                        AllFiles.append("./" + Dir + "/" + File)
+                    else:
+                        pass
+            else:
+                pass
+
+        if len(AllFiles) == 0:
+            print(f"{Color.RED}\t>>> Error: No files found for 3mol_all.txt{Color.RESET}")
+            self.HelpList.append(True)
+        else:
+            print(f"\t>>> 3mol_all.txt files: {Color.GREEN}All Available.{Color.RESET}")
+
+        return AllFiles
+
+    @staticmethod
+    def getOther(AllFile):
+        with open(AllFile, "r") as f:
+            lines = f.readlines()
+        Other_temp = []
+        for line in lines:
+            Contents = line.strip().split()
+            try:
+                Other_temp.append(float(Contents[1]))
+            except (ValueError, IndexError):
+                pass
+        return Other_temp
 
 
 class StandardPhrases:
@@ -114,3 +237,7 @@ class Color:
     BG_WHITE = '\033[47m'  # (background)white
     BG_DEFAULT = '\033[49m'  # (background)default
     RESET = '\033[0m'  # reset
+
+
+if __name__ == "__main__":
+    main()
